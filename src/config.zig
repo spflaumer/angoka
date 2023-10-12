f5a8233169419c34d89205fcc66d406d280bd288
spflaumer <s.pflaumer@murena.io>
2023-10-12 20:51:33 +0000

    create threaded file hashing and expand config structs

--- src/config.zig
+++ src/config.zig
@@ -1,41 +1,68 @@
 const std = @import("std");
 const ArenaAllocator = std.heap.ArenaAllocator;
-const ThreadPool = std.Thread.Pool;
+const Thread = std.Thread;
 const WaitGroup = std.Thread.WaitGroup;
 const ArrayList = std.ArrayList;
+const Mutex = std.Thread.Mutex;
+const sha3_512 = std.crypto.hash.sha3.Sha3_512;
+
+const single_threaded = @import("builtin").single_threaded;
 
 const HashInputPaths = @import("file_hasher.zig").HashInputPaths;
 
-/// contains the state of the thread pool
+/// contains the state of the hashing thread pool
 pub const ThreadContext = struct {
         // thread specific allocator, stays alive until the thread pool is closed
         // used for allocations relating to the thread pool
         arena: ArenaAllocator,
+        // mutex for the arena allocator
+        arena_lock: Mutex,
         // the thread pool containing all the worker threads
-        pool: ThreadPool,
+        threads: []Thread,
+        // the wait group that will be used with the thread pool
+        wait_group: WaitGroup,
+        // the hashing function and it's state
+        hasher: sha3_512,
+        // mutex for the hasher
+        hasher_lock: Mutex,
 
         const Self = @This();
+
+        /// wrapper around pool.spawn() and pool.waitAndWork()
+        /// the function supplied should use this struct's WaitGroup
+        /// this SHOULD NOT be called twice
+        pub fn work(ctx: *Self, comptime func: anytype, args: anytype) !void {
+                // spawn the requested amount of threads
+                for(0..ctx.jobs) |i| ctx.threads[i] = try Thread.spawn(.{}, func, args);
+                // join the threads
+                for (0..ctx.jobs) |i| ctx.threads[i].join();
+        }
 
         /// initialize the thread pool and arena allocator
         pub fn init(ctx: *const AngokuContext, allocator: std.mem.Allocator) !Self {
                 // intialize an ArenaAllocator that will be used by the thread pool
                 var arena = ArenaAllocator.init(allocator);
-                // create the thread pool
-                var pool: *ThreadPool = try arena.allocator().create(ThreadPool);
-                pool.init(.{
-                        .allocator = arena.allocator(),
-                        .n_jobs = ctx.jobs,
-                });
 
                 return .{
                         .arena = arena,
-                        .pool = pool,
+                        .arena_lock = Mutex{},
+                        .threads = try arena.allocator().alloc(Thread, jobs: {
+                                // create only one extern thread if single threaded
+                                if(single_threaded) {
+                                        break :jobs 1;
+                                } else {
+                                        break :jobs ctx.jobs;
+                                }
+                        }),
+                        .wait_group = WaitGroup{},
+                        .hasher = sha3_512.init(.{}),
+                        .hasher_lock = Mutex{},
                 };
         }
 
         /// stop the worker threads and deallocate all used resources
         pub fn deinit(self: *Self) void {
-                self.threads.deinit();
+                self.pool.deinit();
                 self.arena.deinit();
         }
 };
--- src/file_hasher.zig
+++ src/file_hasher.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
-const config = @import("config");
+const ArrayList = std.ArrayList;
 
+const config = @import("config");
+const AngokuConfig = config.AngokuContext;
+const ThreadContext = config.ThreadContext;
+const sha3_512 = std.crypto.hash.sha3.Sha3_512;
 
 pub const HashInputPaths = struct {
         // the path in the filesystem
@@ -8,3 +12,62 @@ pub const HashInputPaths = struct {
         // if the path above would be a folder, specify that
         folder: bool,
 };
+
+inline fn __hashFile(ctx: *ThreadContext, path: []const u8) !void {
+        // open the file
+        const cwd = std.fs.cwd();
+        const file = try cwd.openFile(path, .{});
+        const stat_file = try file.stat();
+
+        // create the buffer where the file's content will end up in
+        ctx.arena_lock.lock();
+        var contents = ctx.arena.allocator().alloc(u8, stat_file.size + 1);
+        ctx.arena_lock.unlock();
+        // read the file into the buffer
+        _ = try file.reader().readAll(contents);
+
+        // add the contents into the hasher state
+        ctx.hasher_lock.lock();
+        ctx.hasher.update(contents);
+        ctx.hasher_lock.unlock();
+}
+
+fn _hashFromPath(ctx: *ThreadContext, inputs: *ArrayList(HashInputPaths)) !void {
+        // get a handle to the current working directory
+        const cwd = std.fs.cwd();
+
+        // get an input path element from the list
+        while(inputs.popOrNull()) |inpu| {
+                var input = @as(HashInputPaths, inpu);
+                switch (input.folder) {
+                        // if the path is a folder
+                        true => {
+                                var iter_folder = try cwd.openIterableDir(input.path, .{});
+                                var walker_folder = try iter_folder.walk(ctx.arena.allocator());
+                                defer walker_folder.deinit();
+                                while(walker_folder.next()) {
+                                        try __hashFile(ctx, input.path);
+                                }
+                        },
+                        else => {
+                                try __hashFile(ctx, input.path);
+                        },
+                }
+        } 
+}
+
+/// get the hash of all input files
+pub fn getHash(ctx: *AngokuConfig) ![sha3_512.digest_length]u8 {
+        // initialize the threads
+        var threads = try ThreadContext.init(ctx, std.heap.page_allocator);
+        defer threads.deinit();
+
+        // spawn the thread pool with the actual hashing function
+        threads.work(_hashFromPath, .{&threads, ctx.input});
+
+        // get the final hash
+        var final_files: [64]u8 = undefined;
+        threads.hasher.final(final_files);
+
+        return final_files;
+}
